3a6b3a0ab4dfc60f03056ea8bc4cd7aad4a43c90
Phoenix Kahlo <phoenix@correlation-one.com>
2019-10-23 14:56:30 -0500

    test truthtable

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,25 @@ pub mod memory;
 pub mod code;
 
 use code::tokens::Token;
+use code::truthtable::IoTruthTable;
 
 fn main() {
+    let mut bitfield = IoTruthTable::yes_unconditional()
+        .pack_bitfield();
+
+    bitfield = !bitfield;
+    bitfield |= IoTruthTable::input_conditional().pack_bitfield();
+
+    println!("{:#?}", bitfield);
+
+    let IoTruthTable(mut byte) = bitfield;
+
+    let mut b2 = IoTruthTable(&mut byte);
+    b2 &= IoTruthTable::new_zeroed_bitfield();
+
+    println!("{:#?}", b2);
+
+    /*
     let code = r##"
     b07afff: ^ y _ n = ~ I ( (( foo foo ^^^ __ "wow!" (( )) )) n * <ff >3 | O (( (( (((()))) )) )) )
     "##;
@@ -31,4 +48,5 @@ fn main() {
             }
         }
     }
+    */
 }
